4e4f05b37bbdbc455e942c2862e711ef7fa4b971
JH Lee <13332150+oulico@users.noreply.github.com>
2023-10-18 19:33:23 +0900

    FIX: pull dev

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,15 +2,29 @@ import NextAuth, {NextAuthOptions} from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import {SupabaseAdapter} from "@auth/supabase-adapter";
 import {Adapter} from "next-auth/adapters";
+import KakaoProvider from "next-auth/providers/kakao";
+import FacebookProvider from "next-auth/providers/facebook";
 
 const handler = NextAuth({
+        // Login Providers,
         providers: [
             GoogleProvider({
                     clientId: process.env.GOOGLE_CLIENT_ID ?? "",
                     clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
                 }
             ),
+            KakaoProvider({
+                    clientId: process.env.KAKAO_CLIENT_ID ?? "",
+                    clientSecret: process.env.KAKAO_CLIENT_SECRET ?? "",
+                },
+            ),
+            FacebookProvider({
+                    clientId: process.env.FACEBOOK_CLIENT_ID ?? "",
+                    clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? "",
+                }
+            ),
         ],
+
         // adapter for supabase
         adapter: SupabaseAdapter({
             url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
@@ -19,7 +33,7 @@ const handler = NextAuth({
     }
 );
 
-
 // if you want to test, sigin in at : http://localhost:3000/api/auth/signin
+// if you want to see the session, go to : https://app.supabase.io/
 
 export {handler as GET, handler as POST}
