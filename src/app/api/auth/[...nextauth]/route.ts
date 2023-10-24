fd085cce79d78a0e6e3622dfb397b14433cb7e23
JH Lee <13332150+oulico@users.noreply.github.com>
2023-10-24 13:27:53 +0900

    FIX: socketio 제거

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import KakaoProvider from "next-auth/providers/kakao";
 import FacebookProvider from "next-auth/providers/facebook";
 import {MongoDBAdapter} from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
 const handler = NextAuth({
         // Login Providers,
@@ -29,6 +30,6 @@ const handler = NextAuth({
 );
 
 // if you want to test, sigin in at : http://localhost:3000/api/auth/signin
-// if you want to see the session, go to : https://app.supabase.io/
+// if you want to see the session, go to : https://cloud.mongodb.com
 
 export {handler as GET, handler as POST}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,95 @@
-"use client"
-import React, {useEffect} from 'react';
-import io from "socket.io-client";
+import Image from 'next/image'
+import styles from './page.module.css'
 
-let socket;
-const Home = () => {
-    const [message, setMessage] = React.useState("");
-    const [userName, setUserName] = React.useState("");
-    const [allMessages, setAllMessages] = React.useState([]);
-
-    async function socketInitializer() {
-        await fetch("/api/socket");
-
-        socket = io();
-
-        socket.on("receive-message", (message) => {
-            setAllMessages((prev) => [...prev, data])
-        });
-    }
-
-    useEffect(() => {
-        socketInitializer();
-
-        return () => {
-            socket?.disconnect();
-        };
-    }, []);
-
-
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        console.log('emitted')
-
-        socket.emit("send-message", {
-            message,
-            userName,
-        });
-
-        setMessage("");
-    }
-
+export default function Home() {
     return (
-        <div>
-            <input value={userName} onChange={(e) => setUserName(e.target.value)}/>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <input name={'message'} placeholder={'message here'} value={message} type="text"
-                           onChange={(e) => setMessage(e.target.value)} autoComplete={'off'}/>
-                    <button type='submit'>send</button>
-                </form>
+        <main className={styles.main}>
+            <div className={styles.description}>
+                <p>
+                    Get started by editing&nbsp;
+                    <code className={styles.code}>src/app/page.tsx</code>
+                </p>
+                <div>
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        By{' '}
+                        <Image
+                            src="/vercel.svg"
+                            alt="Vercel Logo"
+                            className={styles.vercelLogo}
+                            width={100}
+                            height={24}
+                            priority
+                        />
+                    </a>
+                </div>
             </div>
-        </div>
-    );
-};
 
-export default Home;
+            <div className={styles.center}>
+                <Image
+                    className={styles.logo}
+                    src="/next.svg"
+                    alt="Next.js Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
+            </div>
+
+            <div className={styles.grid}>
+                <a
+                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Docs <span>-&gt;</span>
+                    </h2>
+                    <p>Find in-depth information about Next.js features and API.</p>
+                </a>
+
+                <a
+                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Learn <span>-&gt;</span>
+                    </h2>
+                    <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+                </a>
+
+                <a
+                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Templates <span>-&gt;</span>
+                    </h2>
+                    <p>Explore the Next.js 13 playground.</p>
+                </a>
+
+                <a
+                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Deploy <span>-&gt;</span>
+                    </h2>
+                    <p>
+                        Instantly deploy your Next.js site to a shareable URL with Vercel.
+                    </p>
+                </a>
+            </div>
+        </main>
+    )
+}
