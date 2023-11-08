70a39fe1e5a6885999ecca8232b59a6069526ae6
JH Lee <13332150+oulico@users.noreply.github.com>
2023-11-09 08:28:38 +0900

    CHORE: 필요없는 라인 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import styles from './page.module.css'
 import {getServerSession} from "next-auth";
 import {authOptions} from "./api/auth/[...nextauth]/route";
 import React from "react";
-import Link from "next/link";
 import LoginButtons from "@/components/common/LoginButtons";
 
 export default async function Home() {
-    // in server side, you get session by doing this
     const session = await getServerSession(authOptions);
     return (
         <main className={styles.main}>
